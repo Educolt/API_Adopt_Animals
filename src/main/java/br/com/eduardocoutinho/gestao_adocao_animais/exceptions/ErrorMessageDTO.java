0c7cfc138354bffd9654c61d7b3770b4567ba8ef
EduardoCCoutinho <eduardo.coutinho@vcodes.io>
2024-07-16 22:10:02 -0300

    feature: add exceptionHandler

---
+++ src/main/java/br/com/eduardocoutinho/gestao_adocao_animais/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,11 @@
+package br.com.eduardocoutinho.gestao_adocao_animais.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+    private String message;
+    private String field;
+}
